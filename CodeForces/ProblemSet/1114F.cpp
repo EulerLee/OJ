5419527e7ddd90b4c76237649f418d06d3501da8
Euler <euler@localhost.localdomain>
2019-03-15 09:02:16 +0800

    1114

---
+++ CodeForces/ProblemSet/1114F.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define REP(i, n) for(ll i = 0; i < n; ++i)
+#define RANGE(i, x, n) for(ll i = x; i < n; ++i)
+using namespace std;
+typedef long long ll;
+const int INF = 0x3f3f3f3f;
+const ll mod = 1e9 + 7;
+int n, q;
+ll A[400001*2];
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+}
