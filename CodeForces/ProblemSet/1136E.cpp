b6cfbf0aecf431ae70b0900db4f60175cfece84a
Euler <euler19960402@gmail.com>
2019-03-27 16:57:23 +0800

    tmp

--- CodeForces/ProblemSet/1136E.cpp
+++ CodeForces/ProblemSet/1136E.cpp
@@ -6,7 +6,7 @@
 #define RANGE(i, x, n) for(ll i = x; i < n; ++i)
 using namespace std;
 typedef long long ll;
-const ll INF = 0x3f3f3f3f3f3f3f3f;
+const ll INF = 0x9f9f9f9f9f9f9f9f;
 
 const int MAXN = 1<<18;
 ll N, dat[2*MAXN - 1], maxb[2*MAXN - 1], sum[2*MAXN - 1];
@@ -53,43 +53,52 @@ void set_update(int a, int b, ll v, ll l, ll r, ll k)
         sum[k] = v*(r-l);
         return;
     }
-    dat[k] = INF;
-    set_update(a, b, v, l, (l+r)/2, k*2+1);
-    set_update(a, b, v, (l+r)/2, r, k*2+2);
+    if(dat[k] != INF) {
+        dat[k*2+1] = dat[k*2+2] = dat[k];
+        maxb[k*2+1] = maxb[k*2+2] = dat[k];
+        sum[k*2+1] = sum[k*2+2] = sum[k]/2;
+        dat[k] = INF;
+    }
+    set_update(a, b, v, l, (l+r)>>1, k*2+1);
+    set_update(a, b, v, (l+r)>>1, r, k*2+2);
     maxb[k] = max(maxb[k*2+1], maxb[k*2+2]);
-    sum[k] = sum[k*2 + 1] + sum[k*2 + 2];
+    sum[k] = sum[k*2+1] + sum[k*2+2];
 }
 
-int getsum(int a, int b, int k, int l, int r)
+ll getsum(int a, int b, int k, int l, int r)
 {
     if(r <= a || b <= l) return 0;
-    if(l <= a && b <= r) {
-        if(dat[k] != INF) return dat[k]*(b-a);
+    if(a <= l && r <= b) return sum[k];
+    if(dat[k] != INF) {
+        dat[k*2+1] = dat[k*2+2] = dat[k];
+        maxb[k*2+1] = maxb[k*2+2] = dat[k];
+        sum[k*2+1] = sum[k*2+2] = sum[k]/2;
+        dat[k] = INF;
     }
-    if(a <= l && r <= b) {
-        if(dat[k] != INF) {
-            return dat[k]*(r-l);
-        }
-        return sum[k];
-    }
-    if(r - l == 1) return sum[k];
-    else {
-        ll vl = getsum(a, b, k*2 + 1, l, (l+r)/2);
-        ll vr = getsum(a, b, k*2 + 2, (l+r)/2, r);
-        return vl + vr;
-    }
+    return getsum(a, b, k*2+1, l, (l+r)>>1) + getsum(a, b, k*2+2, (l+r)>>1, r);
 }
 
-int findind(int a, int v, int l, int r, int k)
+int findind(int a, int b, int v, int l, int r, int k)
 {
-    if(maxb[k] < v) {
-        return -1;
+    if(b <= a) return -1;
+    if(r <= a || b <= l) return -1;
+    if(maxb[k] < v) return -1;
+    if(r-l == 1) {
+        return l;
     }
-    if(r <= a) return -1;
-    int pl = findind(a, v, (l+r)/2, r, 2*k+2);
-    int pr = findind(a, v, l, (l+r)/2, 2*k+1);
+    if(dat[k] != INF) {
+        dat[k*2+1] = dat[k*2+2] = dat[k];
+        maxb[k*2+1] = maxb[k*2+2] = dat[k];
+        sum[k*2+1] = sum[k*2+2] = sum[k]/2;
+        dat[k] = INF;
+    }
+    int pl = -1, pr = -1;
+    if(maxb[k*2+1] >= v){
+        pl = findind(a, b, v, l, (l+r)>>1, k*2+1);
+    }
     if(pl != -1) return pl;
-    else return pr;
+    pr = findind(a, b, v, (l+r)>>1, r, k*2+2);
+    return pr;
 }
 
 int main()
@@ -100,13 +109,13 @@ int main()
 
     int n, q;
     cin >> n;
-    vector<int> A(n, 0);
-    vector<int> T(n, 0);
+    vector<ll> A(n, 0);
+    vector<ll> T(n, 0);
     REP(i, n) {
         cin >> A[i];
     }
     REP(i, n-1) {
-        int tmp;
+        ll tmp;
         cin >> tmp;
         T[i+1] = T[i]+tmp;
     }
@@ -116,9 +125,9 @@ int main()
     REP(i, n-1) {
         T[i+1] += T[i];
     }
-    cout << endl;
+    /*cout << endl;
     REP(i, n) cout << T[i] << " "; cout << endl;
-    REP(i, n) cout << A[i] << " "; cout << endl;
+    REP(i, n) cout << A[i] << " "; cout << endl;*/
     init(n);
     REP(i, n) {
         update(i, A[i]);
@@ -127,11 +136,12 @@ int main()
     while(q--) {
         if(input<string>() == "+") {
             int k = input<int>();
-            int x = input<int>();
+            int x = input<ll>();
             --k;
             ll bk = getsum(k, k+1, 0, 0, N);
             bk += x;
-            int pos = findind(k, x, 0, N, 0);
+            int pos = findind(k+1, n, bk, 0, N, 0);
+            //cout << k << " " << pos << " " << bk << endl;
             if(pos == -1) {
                 set_update(k, n, bk, 0, N, 0);
             }else {
@@ -143,6 +153,7 @@ int main()
             --l, --r;
             ll ans = getsum(l, r+1, 0, 0, N) + (l==0 ? T[r]:T[r] - T[l-1]);
             cout << ans << endl;
+            //cout << getsum(l, r+1, 0, 0, N) << endl;
         }
     }
 }
