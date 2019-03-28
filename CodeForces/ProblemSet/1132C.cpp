16f60a922483c5ab97b9319cfd4562fcdfc6866d
Euler <euler19960402@gmail.com>
2019-03-28 23:43:25 +0800

    1132C

--- CodeForces/ProblemSet/1132C.cpp
+++ CodeForces/ProblemSet/1132C.cpp
@@ -16,31 +16,48 @@ int main()
 
     int n, q;
     cin >> n >> q;
-    vector<int> sum(n, 0);
-    vector<<pair<int, int>> bd(n, make_pair(0, 0));
+    vector<int> sum(n+1, 0);
+    vector<pair<int, int>> bd(q, make_pair(0, 0));
     REP(i, q) {
         cin >> bd[i].first >> bd[i].second;
         --bd[i].first, --bd[i].second;
-        sum[l]++;
-        sum[r+1]--;
+        sum[bd[i].first]++;
+        sum[bd[i].second+1]--;
     }
-    RANGE(i, 1, n) {
-        sum[i] += sum[i-1];
-    }
-    vector<int> cnt[n];
     REP(i, n) {
-        cnt[i].resize(q);
+        sum[i+1] += sum[i];
+    }
+    //REP(i, n) cout << sum[i] << " ";cout << endl;
+    vector<int> cnt[n+1];
+    REP(i, n+1) {
+        cnt[i].assign(q, 0);
+        if(i == 0) continue;
         REP(j, q) {
-            cnt[i][j] = (sum[i]>j?1:0)+(i==0?0:cnt[i-1][j]);
+            cnt[i][j] = (sum[i-1]>j?1:0)+cnt[i-1][j];
         }
     }
+
+    /*REP(j, q) {
+        cout << j << ": ";
+        RANGE(i, 1, n+1) {
+            cout << cnt[i][j] << " ";
+        }
+        cout << endl;
+    }*/
+
     int ans = 0;
-    REP(i, n) {
-        REP(j, n) {
+    //cout << cnt[n][0] << endl;
+    REP(i, q) {
+        RANGE(j, i+1, q) {
             int l1 = bd[i].first;
             int r1 = bd[i].second;
-            int l2 = bd[i].first;
-            
+            int l2 = bd[j].first;
+            int r2 = bd[j].second;
+            int g1 = (cnt[r1+1][0]-cnt[r1+1][1])-(cnt[l1][0]-cnt[l1][1])+(cnt[r2+1][0]-cnt[r2+1][1])-(cnt[l2][0]-cnt[l2][1]);
+            int g2 = cnt[min(r1, r2)+1][1]-cnt[min(r1, r2)+1][2] - (cnt[max(l1, l2)][1]-cnt[max(l1, l2)][2]);
+            int dec = g1 + (g2 > 0 ? g2: 0);
+            ans = max(ans, cnt[n][0]-dec);
         }
     }
+    cout << ans << endl;
 }
