3359cd39ce8840070827976ef612f95b93dbe93a
Euler <euler19960402@gmail.com>
2019-04-12 12:56:39 +0800

    1107D

--- CodeForces/ProblemSet/1107D.cpp
+++ CodeForces/ProblemSet/1107D.cpp
@@ -15,71 +15,78 @@ void accelerate()
     cout.tie(0);
 }
 
-const int MAXN = 5210;
-int n;
-vector<string> G(MAXN);
-
-int dp[MAXN];
-
-int check(int x)
+int gcd(int a, int b)
 {
-    if(dp[x] != 0) return dp[x];
-    if(x == 1) return dp[x] = 1;
-    if(n%x != 0) return dp[x] = 2;
-
-    int ori = x;
-    for(int i = 2; i*i <= x; ++i) {
-        if(x%i == 0) {
-            if(check(i) == 2) return dp[ori] = 2;
-            x/=i;
-        }
-    }
-    int dvi = ori/x;
-    if(x != 1) {
-        REP(i, n/ori) {
-            REP(j, n/ori) {
-                int st = (G[i*ori][j*ori/4]>>(3-(j*ori)%4))&1;
-                REP(k, x) {
-                    REP(l, x) {
-                        int x1 = i*ori+k*dvi;
-                        int y1 = j*ori+l*dvi;
-                        int v = (G[x1][y1/4]>>(3-y1%4))&1;
-                        if(v != st) return dp[ori] = 2;
-                    }
-                }
-            }
-        }
-        return dp[ori] = 1;
-    }else {
-        return dp[ori] = 1;
-    }
+    if(b == 0) return a;
+    return gcd(b, a%b);
 }
+
 
 int main()
 {
     accelerate();
+    int n;
     cin >> n;
+    vector<string> G(n);    // only need 2 rows
+    int ans = n;
+    string s;
+    // 对每个行列求gcd
     REP(i, n) {
         cin >> G[i];
+        int len = 0;
+        int flag = 0;
         for(auto &x : G[i]) {
             if('0' <= x && x <= '9') x -= '0';
             else x = x - 'A' + 10;
-        }
-    }
-    int ans = 1;
-    RANGE(x, 2, n+1) {
-        if(check(x) == 1) {
-            ans = x;
-        }else {
-            for(int k = 2; k*x <= n; ++k) {
-                dp[k*x] = 2;
+            for(int j = 3; j >= 0; --j) {
+                if(flag == 0) {
+                    if((x>>j)&1) {
+                        ans = gcd(ans, len);
+                        len = 1;
+                        flag = 1;
+                    }else {
+                        ++len;
+                    }
+                }else {
+                    if((x>>j)&1) {
+                        ++len;
+                    }else {
+                        ans = gcd(ans, len);
+                        len = 1;
+                        flag = 0;
+                    }
+                }
             }
         }
+        if(ans == 1) goto end;
+        ans = gcd(ans, len);
     }
+    REP(j, n) {
+        int len = 0;
+        int flag = 0;
+        REP(i, n) {
+            int x = (G[i][j/4]>>(3-j%4))&1;
+            if(flag == 0) {
+                if(x) {
+                    ans = gcd(ans, len);
+                    len = 1;
+                    flag = 1;
+                }else {
+                    ++len;
+                }
+            }else {
+                if(x) {
+                    ++len;
+                }else {
+                    ans = gcd(ans, len);
+                    len = 1;
+                    flag = 0;
+                }
+            }
+        }
+        if(ans == 1) break;
+        ans = gcd(ans, len);
+    }
+end:
     cout << ans << endl;
-    /*if(n == 5200 && G[0][0] == 0) {
-        cout << check(2) << endl;
-        cout << check(13) << endl;
-        cout << check(26) << endl;
-    }*/
 }
