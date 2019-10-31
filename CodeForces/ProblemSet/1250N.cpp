f5cdc624050482a40bf3f593c9d671da41bb35b2
EulerLee <euler19960402@gmail.com>
2019-10-31 20:46:26 +0800

    1250N

--- CodeForces/ProblemSet/1250N.cpp
+++ CodeForces/ProblemSet/1250N.cpp
@@ -24,45 +24,31 @@ typedef long long ll;
 const int MAXN = 1e5+1;
 const int INF = 0x3f3f3f3f;
 unordered_map<int, vector<PII>> G;
-unordered_map<int, int> dep;
 unordered_map<int, int> vis;
-unordered_map<int, int> cut;
-map<PII, int> E;
 vector<PII> res;
 int cnt = 0;
 
-int DFS(int v, int pa = -1, int depth = 0, int root = 1)
+void DFS(int v, int pa = -1, int root = 1)
 {
-    if(vis[v] != 0) return dep[v];
+    if(vis[v] != 0) return;
     vis[v] = 1;
     if(root == 1) {
         ++cnt;
         //cout << v << endl;
     }
-    dep[v] = depth;
-    int actr = depth;
-    int tot = 0;
     int edge = -1;
     for(auto e: G[v]) {
         int u = e.first;
         edge = e.second;
         if(u == pa) continue;
         if(vis[u] == 0) {
-            int tmp = DFS(u, v, depth+1, 0);
-            ++tot;
-            actr = min(actr, tmp);
-            if(root == 1 && tot > 1) cut[v] = 1;
-            if(root == 0 && actr >= depth) cut[v] = 1;
-        }else {
-            actr = min(actr, dep[u]);
+            DFS(u, v, 0);
         }
     }
     vis[v] = 2;
-    cut[v] = 0;
     if(res.size() < cnt) {
         res.emplace_back(edge, v);
     }
-    return actr;
 }
 
 void solve()
@@ -72,8 +58,6 @@ void solve()
     G.clear();
     res.clear();
     vis.clear();
-    cut.clear();
-    dep.clear();
     cnt = 0;
     for(int i = 0; i < n; ++i) {
         int x, y;
@@ -82,7 +66,6 @@ void solve()
         G[x].emplace_back(y, i);
         G[y].emplace_back(x, i);
         vis[x] = vis[y] = 0;
-        E[make_pair(STA(x, y))]++;
     }
     for(auto e: G) {
         int v = e.first;
